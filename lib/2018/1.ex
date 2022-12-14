42596b2f9e4f7676ae52800586f96eb717bd83d8
Roy Veshovda <roy@veshovda.no>
2022-12-14 04:47:00 +0100

    2018 Day 1
    Signed-off-by: Roy Veshovda <roy@veshovda.no>

---
+++ lib/2018/1.ex
@@ -0,0 +1,21 @@
+import AOC
+
+aoc 2018, 1 do
+  def p1(input) do
+    input
+    |> String.split("\n")
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.sum()
+  end
+
+  def p2(input) do
+    input
+    |> String.split("\n")
+    |> Enum.map(&String.to_integer/1)
+    |> Stream.cycle()
+    |> Enum.reduce_while({0, MapSet.new([0])}, fn val, {sum , set} ->
+      sum = sum + val
+      if sum in set, do: {:halt, sum}, else: {:cont, {sum, MapSet.put(set, sum)}}
+    end)
+  end
+end
