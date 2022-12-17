0c4d4ff2927ae7f4d56f444718ee58d84b9f4c01
Roy Veshovda <roy@veshovda.no>
2022-12-17 07:59:54 +0100

    Fast forward
    Signed-off-by: Roy Veshovda <roy@veshovda.no>

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,19 @@
 import Config
 
+try do
+  File.stream!("./.env")
+    |> Stream.map(&String.trim_trailing/1) # remove excess whitespace
+    |> Enum.each(fn line -> line           # loop through each line
+      |> String.replace("export ", "")     # remove "export" from line
+      |> String.split("=")                 # split on the "=" (equals sign)
+      |> Enum.reduce(fn(value, key) ->     # stackoverflow.com/q/33055834/1148249
+        System.put_env(key, value)         # set each environment variable
+      end)
+    end)
+rescue
+  _ -> IO.puts "no .env file found!"
+end
+
 aoc_token =
   System.get_env("AOC_TOKEN") ||
     raise """
