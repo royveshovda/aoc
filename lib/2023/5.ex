13d50ec138721fbf2fed1ea50d4f353020e3c421
Roy Veshovda <roy@veshovda.no>
2023-12-08 05:42:53 +0100

    D5 - P2
    Signed-off-by: Roy Veshovda <roy@veshovda.no>

--- lib/2023/5.ex
+++ lib/2023/5.ex
@@ -37,40 +37,31 @@ aoc 2023, 5 do
       46
 
       iex> p2(input_string())
-      123
+      20283860
   """
   def p2(input) do
-    map =
+    data =
       input
-      |> parse()
+      |> String.split("\n")
+      |> Enum.reverse
+      |> tl
+      |> Enum.reverse
 
-    map.seeds
-    |> Enum.chunk_every(2, 2, :discard)
-    |> Enum.with_index()
-    |> Enum.map(fn {[seed, range], index} ->
-      IO.puts("Start index #{index} with seed #{seed} and range #{range}")
-      res =
-        seed..(seed + range + 1)
-        |> Enum.map(fn number ->
-          if rem(number, 10000000) == 0 do
-            IO.puts("Index: #{index} -- Number #{number} (Start: #{seed}) -- End: #{seed + range})")
-          end
+    {:ok, seeds, _, _, _, _} =
+      data
+      |> hd
+      |> Aoc23.Day05Parser.parseSeeds
 
-          number
-          |> p1_next_number(map.seed_to_soil_map)
-          |> p1_next_number(map.soil_to_fertilizer_map)
-          |> p1_next_number(map.fertilizer_to_water_map)
-          |> p1_next_number(map.water_to_light_map)
-          |> p1_next_number(map.light_to_temperature_map)
-          |> p1_next_number(map.temperature_to_humidity_map)
-          |> p1_next_number(map.humidity_to_location_map)
-        end)
-        |> Enum.min()
+    data = tl(tl(data))
 
-      IO.puts("Index #{index} has result #{res}")
-      res
-    end)
-    |> Enum.min()
+    stages = generate_stages(data, [])
+
+    pairs = gen_pairs(seeds)
+
+    pairs
+    |> Enum.flat_map(fn pair -> progress_seed(pair, stages) end)
+    |> Enum.map(fn {k, _} -> k end)
+    |> Enum.min
   end
 
   def p1_next_number(number, map) do
@@ -128,4 +119,104 @@ aoc 2023, 5 do
     |> String.split(" ", trim: true)
     |> Enum.map(&String.to_integer/1)
   end
+
+  # Part 2
+  def progress_seed(seed, stages) do
+      for stage <- stages, reduce: [seed] do
+        acc -> Enum.flat_map(acc, fn item -> progress_stage(item, stage,[]) end)
+      end
+    end
+
+    def progress_stage({key, len}, stage, result) do
+      {src, dest, stage_len} = lookup(stage, {key, len})
+      if (src + stage_len) >= (key + len) do
+        [{dest + (key - src), len} | result]
+      else
+        consumed_len = (src + stage_len) - key
+        progress_stage({key + consumed_len, len - consumed_len}, stage, [{dest + (key - src), consumed_len} | result])
+      end
+    end
+
+    # Binary search to find the correct transform for a key in a stage
+    def lookup(stage, seed), do: lookup(stage, seed, 0, Arrays.size(stage) - 1)
+    def lookup(stage, {seed_low, seed_len}, l, r) do
+      if l > r do
+        {seed_low, seed_low, seed_len}
+      else
+        m = div(l + r, 2)
+        {src, _, stage_len} = stage[m]
+        cond do
+          src + stage_len <= seed_low -> lookup(stage, {seed_low, seed_len}, m + 1, r)
+          src > seed_low -> lookup(stage, {seed_low, seed_len}, l, m - 1)
+          true -> stage[m]
+        end
+      end
+    end
+
+  def generate_stages(nil, result), do: Enum.reverse(result)
+    def generate_stages(data, result) do
+      {stage, remaining} = process_stage(data)
+      generate_stages(remaining, [stage | result])
+    end
+
+    def gen_pairs(seeds), do: gen_pairs(seeds, [])
+    def gen_pairs([], result), do: result
+    def gen_pairs([l | [len | rest]], result), do: gen_pairs(rest, [{l, len} | result])
+
+    def fill_stage(stage), do: fill_stage(stage, 0, [])
+    def fill_stage([], _, result), do: result
+    def fill_stage([{s, d, l} | rest], current, result) do
+      if (current < s) do
+        fill_stage(rest, s + l, [{s,d,l} | [{current, current, s - current} | result]])
+      else
+        fill_stage(rest, s + l, [{s,d,l} | result])
+      end
+    end
+
+    def finalize_stage(stage, data) do
+      stage
+      |> Enum.sort(fn {s1, _, _}, {s2, _, _} -> s1 <= s2 end)
+      |> fill_stage
+      |> Enum.reverse
+      |> Enum.into(Arrays.new())
+      |> then(fn arr -> {arr, data} end)
+    end
+
+    def process_stage(data), do: process_stage(tl(data), [])
+    def process_stage([], result), do: finalize_stage(result, nil)
+    def process_stage([line | rest], result) do
+      case line do
+        "" ->
+          finalize_stage(result, rest)
+        _ ->
+          {:ok, parsed_line, _, _, _, _} = Aoc23.Day05Parser.parseMap line
+          process_stage(rest, [transform_line(parsed_line) | result])
+      end
+    end
+
+    def transform_line([dest, source, len]), do: {source, dest, len}
+end
+
+defmodule Aoc23.Day05Parser do
+  import NimbleParsec
+
+  whitespace =
+    repeat(ignore(string(" ")))
+
+  number =
+  whitespace
+  |> concat(integer(min: 1))
+  |> concat(whitespace)
+
+  seeds =
+  ignore(string("seeds:"))
+  |> concat(repeat(number))
+
+  map =
+    number
+    |> concat(number)
+    |> concat(number)
+
+  defparsec(:parseSeeds, seeds)
+  defparsec(:parseMap, map)
 end
--- mix.exs
+++ mix.exs
@@ -14,6 +14,7 @@ defmodule Aoc.MixProject do
     [
       {:advent_of_code_utils, "~> 4.0"},
       {:nimble_parsec, "~> 1.4"},
+      {:arrays, "~> 2.1.1"},
       {:heap, "~> 3.0"},
       {:qex, "~> 0.5"},
       {:mix_test_watch, "~> 1.1.1", only: :dev}
