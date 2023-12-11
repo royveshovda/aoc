7f2300bd9a0284f43f964ab7fd41eab6695d9840
Roy Veshovda <roy@veshovda.no>
2023-12-11 05:36:07 +0100

    D11 preparation
    Signed-off-by: Roy Veshovda <roy@veshovda.no>

---
+++ lib/2023/11.ex
@@ -0,0 +1,29 @@
+import AOC
+
+aoc 2023, 11 do
+  @moduledoc """
+  https://adventofcode.com/2023/day/11
+  """
+
+  @doc """
+      iex> p1(example_string())
+      123
+
+      #iex> p1(input_string())
+      #123
+  """
+  def p1(input) do
+    input
+  end
+
+  @doc """
+      #iex> p2(example_string())
+      #123
+
+      #iex> p2(input_string())
+      #123
+  """
+  def p2(input) do
+    input
+  end
+end
