4b5a1f3e67f490729ae5aea4650fc0ecc9292af6
Roy Veshovda <roy@veshovda.no>
2023-12-13 18:35:46 +0100

    D14 - preparation
    Signed-off-by: Roy Veshovda <roy@veshovda.no>

---
+++ lib/2023/14.ex
@@ -0,0 +1,35 @@
+import AOC
+
+aoc 2023, 14 do
+  @moduledoc """
+  https://adventofcode.com/2023/day/14
+  """
+
+  @doc """
+      iex> p1(example_string())
+      123
+
+      #iex> p1(input_string())
+      #123
+  """
+  def p1(input) do
+    input
+    |> parse()
+  end
+
+  def parse(input) do
+    input
+  end
+
+  @doc """
+      #iex> p2(example_string())
+      #123
+
+      #iex> p2(input_string())
+      #123
+  """
+  def p2(input) do
+    input
+    |> parse()
+  end
+end
