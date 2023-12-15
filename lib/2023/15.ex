1986f37b19c5cf5a06131ae0fea5f18b0e50c4b8
Roy Veshovda <roy@veshovda.no>
2023-12-15 06:19:39 +0100

    D15 - part 1
    Signed-off-by: Roy Veshovda <roy@veshovda.no>

--- lib/2023/15.ex
+++ lib/2023/15.ex
@@ -7,14 +7,29 @@ aoc 2023, 15 do
 
   @doc """
       iex> p1(example_string())
-      123
+      1320
 
-      #iex> p1(input_string())
-      #123
+      iex> p1(input_string())
+      505379
   """
   def p1(input) do
     input
     |> parse()
+    |> Enum.map(fn line ->
+      line
+      |> run_hash()
+    end)
+    |> Enum.sum()
+  end
+
+  def run_hash(string) do
+    vals = to_charlist(string)
+    Enum.reduce(vals, 0, fn val, acc ->
+      s1 = acc + val
+      s2 = s1 * 17
+      s3 = rem(s2, 256)
+      s3
+    end)
   end
 
   @doc """
@@ -31,5 +46,6 @@ aoc 2023, 15 do
 
   def parse(input) do
     input
+    |> String.split(",")
   end
 end
