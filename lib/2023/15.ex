29a3983308d16c5913ce053d68de94f6c41ce7a2
Roy Veshovda <roy@veshovda.no>
2023-12-15 07:14:02 +0100

    D15 - part 2
    Signed-off-by: Roy Veshovda <roy@veshovda.no>

--- lib/2023/15.ex
+++ lib/2023/15.ex
@@ -33,15 +33,69 @@ aoc 2023, 15 do
   end
 
   @doc """
-      #iex> p2(example_string())
-      #123
+      iex> p2(example_string())
+      145
 
-      #iex> p2(input_string())
-      #123
+      iex> p2(input_string())
+      263211
   """
   def p2(input) do
     input
     |> parse()
+    |> prepare_labels()
+    |> Enum.group_by(fn {_, _, _, box} -> box end)
+    |> Enum.map(fn {box_id, operations} ->
+      process_box(box_id, operations)
+    end)
+    |> Enum.map(&calculate_score/1)
+    |> Enum.sum()
+  end
+
+  def calculate_score({box_id, values}) do
+    box = box_id + 1
+    Enum.with_index(values, 1)
+    |> Enum.map(fn {{_label, focal}, index} ->
+      focal * index * box
+    end)
+    |> Enum.sum()
+  end
+
+  def process_box(box_id, operations) do
+    values =
+      Enum.reduce(operations, [], fn {label, op, focal, _hash}, acc ->
+        case op do
+          :minus ->
+            res = Enum.reject(acc, fn {l, _} -> l == label end)
+            res
+          :equal ->
+            case Enum.any?(acc, fn {l, _} -> l == label end) do
+              true ->
+                Enum.map(acc, fn {l, _} = current ->
+                  if l == label do
+                    {l, focal}
+                  else
+                    current
+                  end
+                end)
+              false ->
+                List.insert_at(acc, -1, {label, focal})
+            end
+        end
+      end)
+    {box_id, values}
+  end
+
+  def prepare_labels(operations) do
+    Enum.map(operations, fn operation ->
+      case String.contains?(operation, "-") do
+        true ->
+          [label] = String.split(operation, "-", trim: true)
+          {label, :minus, 0, run_hash(label)}
+        false ->
+          [label, value] = String.split(operation, "=")
+          {label, :equal, String.to_integer(value), run_hash(label)}
+      end
+    end)
   end
 
   def parse(input) do
