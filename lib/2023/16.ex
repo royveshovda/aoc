11a06c3ad355bfd3227d3b78963144cdcee2eca5
Roy Veshovda <roy@veshovda.no>
2023-12-16 08:00:38 +0100

    D16
    Signed-off-by: Roy Veshovda <roy@veshovda.no>

--- lib/2023/16.ex
+++ lib/2023/16.ex
@@ -6,27 +6,87 @@ aoc 2023, 16 do
   """
 
   @doc """
-      #iex> p1(example_string())
-      #123
+      iex> p1(example_string())
+      46
 
-      #iex> p1(input_string())
-      #123
+      iex> p1(input_string())
+      7608
   """
   def p1(input) do
-    input
-    |> parse_input()
+    start = [{:right, 0, 0}]
+
+    grid =
+      input
+      |> parse_input()
+
+    beam(grid, start, [])
+    |> Enum.map(fn {_, row, col} -> {row, col} end)
+    |> Enum.uniq()
+    |> Enum.count()
   end
 
   @doc """
-      #iex> p2(example_string())
-      #123
+      iex> p2(example_string())
+      51
 
-      #iex> p2(input_string())
-      #123
+      iex> p2(input_string())
+      8221
   """
   def p2(input) do
-    input
-    |> parse_input()
+    grid =
+      input
+      |> parse_input()
+
+    generate_start_points(grid)
+    |> Enum.with_index()
+    |> Enum.map(fn {start, index} ->
+      IO.puts("#{index}")
+      beam(grid, [start], [])
+      |> Enum.map(fn {_, row, col} -> {row, col} end)
+      |> Enum.uniq()
+      |> Enum.count()
+    end)
+    |> Enum.max()
+  end
+
+  def generate_start_points(grid) do
+    max_col = grid |> Enum.map(fn {{_, col}, _} -> col end) |> Enum.max()
+    max_row = grid |> Enum.map(fn {{row, _}, _} -> row end) |> Enum.max()
+
+    top_edge = for col <- 0..max_col, into: [] do
+      {:down, 0, col}
+    end
+
+    bottom_edge = for col <- 0..max_col, into: [] do
+      {:up, max_row, col}
+    end
+
+    left_edge = for row <- 0..max_row, into: [] do
+      {:right, row, 0}
+    end
+
+    right_edge = for row <- 0..max_row, into: [] do
+      {:left, row, max_col}
+    end
+
+    edges = top_edge ++ bottom_edge ++ left_edge ++ right_edge
+    IO.puts("#{inspect(length(edges))}")
+    edges
+  end
+
+  def beam(_grid, [], energized) do
+    energized
+  end
+
+  def beam(grid, [{_d, row, col} = move | moves], energized) do
+    case move in energized do
+      true -> beam(grid, moves, energized)
+      _ ->
+        case Map.has_key?(grid, {row, col}) do
+          true -> beam(grid, move(move, grid[{row, col}]) ++ moves, [move|energized])
+          _ -> beam(grid, moves, energized)
+        end
+    end
   end
 
   def parse_input(input) do
@@ -37,100 +97,24 @@ aoc 2023, 16 do
     end
   end
 
-  def move({:right, row, col}, ".", {max_row, max_col}) do
-    [{:right, row, col + 1}] |> roll_over({max_row, max_col})
-  end
-
-  def move({:left, row, col}, ".", {max_row, max_col}) do
-    [{:left, row, col - 1}] |> roll_over({max_row, max_col})
-  end
-
-  def move({:down, row, col}, ".", {max_row, max_col}) do
-    [{:down, row + 1, col}] |> roll_over({max_row, max_col})
-  end
-
-  def move({:up, row, col}, ".", {max_row, max_col}) do
-    [{:up, row - 1, col}] |> roll_over({max_row, max_col})
-  end
-
-  def move({:right, row, col}, "/", {max_row, max_col}) do
-    [{:up, row - 1, col}] |> roll_over({max_row, max_col})
-  end
-
-  def move({:left, row, col}, "/", {max_row, max_col}) do
-    [{:down, row + 1, col}] |> roll_over({max_row, max_col})
-  end
-
-  def move({:down, row, col}, "/", {max_row, max_col}) do
-    [{:left, row, col - 1}] |> roll_over({max_row, max_col})
-  end
-
-  def move({:up, row, col}, "/", {max_row, max_col}) do
-    [{:right, row, col + 1}] |> roll_over({max_row, max_col})
-  end
-
-  def move({:right, row, col}, "\\", {max_row, max_col}) do
-    [{:down, row + 1, col}] |> roll_over({max_row, max_col})
-  end
-
-  def move({:left, row, col}, "\\", {max_row, max_col}) do
-    [{:up, row - 1, col}] |> roll_over({max_row, max_col})
-  end
-
-  def move({:down, row, col}, "\\", {max_row, max_col}) do
-    [{:right, row, col + 1}] |> roll_over({max_row, max_col})
-  end
-
-  def move({:up, row, col}, "\\", {max_row, max_col}) do
-    [{:left, row, col - 1}] |> roll_over({max_row, max_col})
-  end
-
-  def move({:right, row, col}, "-", {max_row, max_col}) do
-    [{:right, row, col + 1}] |> roll_over({max_row, max_col})
-  end
-
-  def move({:left, row, col}, "-", {max_row, max_col}) do
-    [{:left, row, col - 1}] |> roll_over({max_row, max_col})
-  end
-
-  def move({:down, row, col}, "-", {max_row, max_col}) do
-    [{:left, row, col - 1}, {:right, row, col + 1}] |> roll_over({max_row, max_col})
-  end
-
-  def move({:up, row, col}, "-", {max_row, max_col}) do
-    [{:left, row, col - 1}, {:right, row, col + 1}] |> roll_over({max_row, max_col})
-  end
-
-  def move({:right, row, col}, "|", {max_row, max_col}) do
-    [{:up, row - 1, col}, {:down, row + 1, col}] |> roll_over({max_row, max_col})
-  end
-
-  def move({:left, row, col}, "|", {max_row, max_col}) do
-    [{:up, row - 1, col}, {:down, row + 1, col}] |> roll_over({max_row, max_col})
-  end
-
-  def move({:down, row, col}, "|", {max_row, max_col}) do
-    [{:down, row + 1, col}] |> roll_over({max_row, max_col})
-  end
-
-  def move({:up, row, col}, "|", {max_row, max_col}) do
-    [{:up, row - 1, col}] |> roll_over({max_row, max_col})
-  end
-
-
-  def roll_over(points, {max_row, max_col}) do
-    Enum.map(points, fn {dir, row, col} ->
-      single_roll_over({dir, row, col}, {max_row, max_col}) end)
-  end
-
-
-  def single_roll_over({dir, row, col}, {max_row, max_col}) do
-    case {row, col} do
-      {row, col} when row < 0 -> {dir, max_row, col}
-      {row, col} when row > max_row -> {dir, 0, col}
-      {row, col} when col < 0 -> {dir, row, max_col}
-      {row, col} when col > max_col -> {dir, row, 0}
-      {row, col} -> {dir, row, col}
-    end
-  end
+  def move({:right, row, col}, "."), do: [{:right, row, col + 1}]
+  def move({:left, row, col}, "."), do: [{:left, row, col - 1}]
+  def move({:down, row, col}, "."), do: [{:down, row + 1, col}]
+  def move({:up, row, col}, "."), do: [{:up, row - 1, col}]
+  def move({:right, row, col}, "/"), do: [{:up, row - 1, col}]
+  def move({:left, row, col}, "/"), do: [{:down, row + 1, col}]
+  def move({:down, row, col}, "/"), do: [{:left, row, col - 1}]
+  def move({:up, row, col}, "/"), do: [{:right, row, col + 1}]
+  def move({:right, row, col}, "\\"), do: [{:down, row + 1, col}]
+  def move({:left, row, col}, "\\"), do: [{:up, row - 1, col}]
+  def move({:down, row, col}, "\\"), do: [{:right, row, col + 1}]
+  def move({:up, row, col}, "\\"), do: [{:left, row, col - 1}]
+  def move({:right, row, col}, "-"), do: [{:right, row, col + 1}]
+  def move({:left, row, col}, "-"), do: [{:left, row, col - 1}]
+  def move({:down, row, col}, "-"), do: [{:left, row, col - 1}, {:right, row, col + 1}]
+  def move({:up, row, col}, "-"), do: [{:left, row, col - 1}, {:right, row, col + 1}]
+  def move({:right, row, col}, "|"), do: [{:up, row - 1, col}, {:down, row + 1, col}]
+  def move({:left, row, col}, "|"), do: [{:up, row - 1, col}, {:down, row + 1, col}]
+  def move({:down, row, col}, "|"), do: [{:down, row + 1, col}]
+  def move({:up, row, col}, "|"), do: [{:up, row - 1, col}]
 end
--- test/2023/16_test.exs
+++ test/2023/16_test.exs
@@ -2,98 +2,82 @@ import AOC
 
 aoc_test 2023, 16, async: true do
   test "Move right into ." do
-    assert Y2023.D16.move({:right, 1, 1}, ".", {10,10}) == [{:right, 1, 2}]
+    assert Y2023.D16.move({:right, 1, 1}, ".") == [{:right, 1, 2}]
   end
 
   test "Move left into ." do
-    assert Y2023.D16.move({:left, 1, 1}, ".", {10,10}) == [{:left, 1, 0}]
+    assert Y2023.D16.move({:left, 1, 1}, ".") == [{:left, 1, 0}]
   end
 
   test "Move down into ." do
-    assert Y2023.D16.move({:down, 1, 1}, ".", {10,10}) == [{:down, 2, 1}]
+    assert Y2023.D16.move({:down, 1, 1}, ".") == [{:down, 2, 1}]
   end
 
   test "Move up into ." do
-    assert Y2023.D16.move({:up, 1, 1}, ".", {10,10}) == [{:up, 0, 1}]
-  end
-
-  test "Move right into . with roll over" do
-    assert Y2023.D16.move({:right, 1, 10}, ".", {10,10}) == [{:right, 1, 0}]
-  end
-
-  test "Move left into . with roll over" do
-    assert Y2023.D16.move({:left, 1, 0}, ".", {10,10}) == [{:left, 1, 10}]
-  end
-
-  test "Move down into . with roll over" do
-    assert Y2023.D16.move({:down, 10, 1}, ".", {10,10}) == [{:down, 0, 1}]
-  end
-
-  test "Move up into . with roll over" do
-    assert Y2023.D16.move({:up, 0, 1}, ".", {10,10}) == [{:up, 10, 1}]
+    assert Y2023.D16.move({:up, 1, 1}, ".") == [{:up, 0, 1}]
   end
 
   test "Move right into /" do
-    assert Y2023.D16.move({:right, 1, 1}, "/", {10,10}) == [{:up, 0, 1}]
+    assert Y2023.D16.move({:right, 1, 1}, "/") == [{:up, 0, 1}]
   end
 
   test "Move left into /" do
-    assert Y2023.D16.move({:left, 1, 1}, "/", {10,10}) == [{:down, 2, 1}]
+    assert Y2023.D16.move({:left, 1, 1}, "/") == [{:down, 2, 1}]
   end
 
   test "Move down into /" do
-    assert Y2023.D16.move({:down, 1, 1}, "/", {10,10}) == [{:left, 1, 0}]
+    assert Y2023.D16.move({:down, 1, 1}, "/") == [{:left, 1, 0}]
   end
 
   test "Move up into /" do
-    assert Y2023.D16.move({:up, 1, 1}, "/", {10,10}) == [{:right, 1, 2}]
+    assert Y2023.D16.move({:up, 1, 1}, "/") == [{:right, 1, 2}]
   end
 
   test "Move right into \\" do
-    assert Y2023.D16.move({:right, 1, 1}, "\\", {10,10}) == [{:down, 2, 1}]
+    assert Y2023.D16.move({:right, 1, 1}, "\\") == [{:down, 2, 1}]
   end
 
   test "Move left into \\" do
-    assert Y2023.D16.move({:left, 1, 1}, "\\", {10,10}) == [{:up, 0, 1}]
+    assert Y2023.D16.move({:left, 1, 1}, "\\") == [{:up, 0, 1}]
   end
 
   test "Move down into \\" do
-    assert Y2023.D16.move({:down, 1, 1}, "\\", {10,10}) == [{:right, 1, 2}]
+    assert Y2023.D16.move({:down, 1, 1}, "\\") == [{:right, 1, 2}]
   end
 
   test "Move up into \\" do
-    assert Y2023.D16.move({:up, 1, 1}, "\\", {10,10}) == [{:left, 1, 0}]
+    assert Y2023.D16.move({:up, 1, 1}, "\\") == [{:left, 1, 0}]
   end
 
   test "Move right into -" do
-    assert Y2023.D16.move({:right, 1, 1}, "-", {10,10}) == [{:right, 1, 2}]
+    assert Y2023.D16.move({:right, 1, 1}, "-") == [{:right, 1, 2}]
   end
 
   test "Move left into -" do
-    assert Y2023.D16.move({:left, 1, 1}, "-", {10,10}) == [{:left, 1, 0}]
+    assert Y2023.D16.move({:left, 1, 1}, "-") == [{:left, 1, 0}]
   end
 
   test "Move down into -" do
-    assert Y2023.D16.move({:down, 1, 1}, "-", {10,10}) == [{:left, 1, 0}, {:right, 1, 2}]
+    assert Y2023.D16.move({:down, 1, 1}, "-") == [{:left, 1, 0}, {:right, 1, 2}]
   end
 
   test "Move up into -" do
-    assert Y2023.D16.move({:up, 1, 1}, "-", {10,10}) == [{:left, 1, 0}, {:right, 1, 2}]
+    assert Y2023.D16.move({:up, 1, 1}, "-") == [{:left, 1, 0}, {:right, 1, 2}]
   end
 
   test "Move right into |" do
-    assert Y2023.D16.move({:right, 1, 1}, "|", {10,10}) == [{:up, 0, 1}, {:down, 2, 1}]
+    assert Y2023.D16.move({:right, 1, 1}, "|") == [{:up, 0, 1}, {:down, 2, 1}]
   end
 
   test "Move left into |" do
-    assert Y2023.D16.move({:left, 1, 1}, "|", {10,10}) == [{:up, 0, 1}, {:down, 2, 1}]
+    assert Y2023.D16.move({:left, 1, 1}, "|") == [{:up, 0, 1}, {:down, 2, 1}]
   end
 
   test "Move down into |" do
-    assert Y2023.D16.move({:down, 1, 1}, "|", {10,10}) == [{:down, 2, 1}]
+    assert Y2023.D16.move({:down, 1, 1}, "|") == [{:down, 2, 1}]
   end
 
   test "Move up into |" do
-    assert Y2023.D16.move({:up, 1, 1}, "|", {10,10}) == [{:up, 0, 1}]
+    assert Y2023.D16.move({:up, 1, 1}, "|") == [{:up, 0, 1}]
   end
 end
