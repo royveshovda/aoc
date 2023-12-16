cbae7ea12e39347e5cfcc1fed08c76d039279f15
Roy Veshovda <roy@veshovda.no>
2023-12-16 08:03:29 +0100

    D17 - prepare
    Signed-off-by: Roy Veshovda <roy@veshovda.no>

---
+++ lib/2023/17.ex
@@ -0,0 +1,36 @@
+import AOC
+
+aoc 2023, 17 do
+  @moduledoc """
+  https://adventofcode.com/2023/day/17
+  """
+
+  @doc """
+      iex> p1(example_string())
+      123
+
+      #iex> p1(input_string())
+      #123
+  """
+  def p1(input) do
+    input
+    |> parse_input()
+  end
+
+  @doc """
+      #iex> p2(example_string())
+      #123
+
+      #iex> p2(input_string())
+      #123
+  """
+  def p2(input) do
+    input
+    |> parse_input()
+  end
+
+  def parse_input(input) do
+    input
+    |> String.split("\n")
+  end
+end
---
+++ test/2023/17_test.exs
@@ -0,0 +1,4 @@
+import AOC
+
+aoc_test 2023, 17, async: true do
+end
