20e3303b711c6738310516a664017c84477c0894
Roy Veshovda <roy@veshovda.no>
2023-12-18 12:25:17 +0100

    D18 - part 2
    Signed-off-by: Roy Veshovda <roy@veshovda.no>

--- lib/2023/18.ex
+++ lib/2023/18.ex
@@ -15,118 +15,92 @@ aoc 2023, 18 do
   def p1(input) do
     instructions =
       input
-      |> parse_input()
+      |> parse_input_p1()
 
-    {edge, _stop_position} = create_edge(instructions)
-
-    edge_length = length(edge |> Map.keys())
-    IO.puts("edge length: #{edge_length}")
-    fill = flood_fill(edge, [{1,1}], %{})
-    fill_length = length(fill |> Map.keys())
-
-    edge_length + fill_length
-    #print_edge(edge)
+    {edge, edge_length} = calculate_edge_length_and_polygons(instructions)
+    fill_size = shoelace_area(edge)
+    floor((edge_length / 2) + fill_size)
   end
 
-  def flood_fill(_edge, [], fill) do
-    fill
+  def calculate_edge_length_and_polygons(instructions) do
+    {edge, _stop_position, edge_length} =
+      instructions
+      |> Enum.reduce({[], {0,0}, 0}, fn {op, length}, {edge, {current_row, current_col}, edge_length} ->
+        new_edge_length = edge_length + length
+        next_pos =
+          case op do
+            "R" ->
+              {current_row, current_col + length}
+            "L" ->
+              {current_row, current_col - length}
+            "D" ->
+              {current_row + length, current_col}
+            "U" ->
+              {current_row - length, current_col}
+          end
+        new_edge = [{current_row, current_col} | edge]
+        {new_edge, next_pos, new_edge_length}
+      end)
+    {edge, edge_length}
   end
 
-  def flood_fill(edge, [{row, col} | rest], fill) do
-    case Map.has_key?(fill, {row, col}) do
-      true -> flood_fill(edge, rest, fill)
-      false ->
-        new_next =
-          [{row - 1, col}, {row + 1, col}, {row, col - 1}, {row, col + 1}]
-          |> Enum.filter(fn p -> !Map.has_key?(fill, p) end)
-          |> Enum.filter(fn p -> !Map.has_key?(edge, p) end)
-        new_fill = Map.put(fill, {row, col}, "#")
-        flood_fill(edge, rest ++ new_next, new_fill)
-    end
-  end
-
-  def print_edge(edge) do
-    IO.puts("print edge")
-    min_row = edge |> Map.keys() |> Enum.map(fn {row, _} -> row end) |> Enum.min()
-    max_row = edge |> Map.keys() |> Enum.map(fn {row, _} -> row end) |> Enum.max()
-    min_col = edge |> Map.keys() |> Enum.map(fn {_, col} -> col end) |> Enum.min()
-    max_col = edge |> Map.keys() |> Enum.map(fn {_, col} -> col end) |> Enum.max()
-
-    for row <- min_row..max_row do
-      for col <- min_col..max_col do
-        case Map.get(edge, {row, col}) do
-          nil -> IO.write(".")
-          #color -> IO.write(color)
-          _color -> IO.write("#")
-        end
-      end
-      IO.puts("")
-    end
-  end
-
-  def create_edge(instructions) do
-    instructions
-    |> Enum.reduce({%{}, {0,0}}, fn {op, length, color}, {edge, {current_row, current_col}} ->
-      case op do
-        "R" ->
-          new_map =
-            1..length
-            |> Enum.map(fn i ->
-              {{current_row, current_col + i}, color}
-            end)
-            |> Enum.into(edge)
-          {new_map, {current_row, current_col + length}}
-        "L" ->
-          new_map =
-            1..length
-            |> Enum.map(fn i ->
-              {{current_row, current_col - i}, color}
-            end)
-            |> Enum.into(edge)
-          {new_map, {current_row, current_col - length}}
-        "D" ->
-          new_map =
-            1..length
-            |> Enum.map(fn i ->
-              {{current_row + i, current_col}, color}
-            end)
-            |> Enum.into(edge)
-          {new_map, {current_row + length, current_col}}
-        "U" ->
-          new_map =
-            1..length
-            |> Enum.map(fn i ->
-              {{current_row - i, current_col}, color}
-            end)
-            |> Enum.into(edge)
-          {new_map, {current_row - length, current_col}}
-        _ ->
-          {edge, {current_row, current_col}}
-      end
+  def shoelace_area(polygons) do
+    n = length(polygons)
+    0..n-1
+    |> Enum.map(fn i ->
+      {x1, y1} = polygons |> Enum.at(i)
+      {x2, y2} = polygons |> Enum.at(rem(i + 1, n))
+      x1 * y2 - x2 * y1
     end)
+    |> Enum.sum()
+    |> then(fn x -> (x / 2) + 1 end)
   end
 
   @doc """
-      #iex> p2(example_string())
-      #123
+      iex> p2(example_string())
+      952408144115
 
-      #iex> p2(input_string())
-      #123
+      iex> p2(input_string())
+      66296566363189
   """
   def p2(input) do
-    input
-    |> parse_input()
+    instructions =
+      input
+      |> parse_input_p2()
+
+    {edge, edge_length} = calculate_edge_length_and_polygons(instructions)
+    fill_size = shoelace_area(edge)
+    floor((edge_length / 2) + fill_size)
   end
 
-  def parse_input(input) do
+  def parse_input_p1(input) do
     input
     |> String.split("\n")
     |> Enum.map(&String.split(&1, " "))
-    |> Enum.map(&parse_line/1)
+    |> Enum.map(&parse_line_p1/1)
   end
 
-  def parse_line([op, length, color]) do
+  def parse_line_p1([op, length, _color]) do
+    {op, String.to_integer(length)}
+  end
+
+  def parse_input_p2(input) do
+    input
+    |> String.split("\n")
+    |> Enum.map(&String.split(&1, " "))
+    |> Enum.map(&parse_line_p2/1)
+  end
+
+  def parse_line_p2([_fake_op, _fake_length, color]) do
     color = color |> String.trim("(") |> String.trim(")") |> String.trim("#")
-    {op, String.to_integer(length), color}
+    length = String.slice(color, 0..4) |> String.to_integer(16)
+    op =
+      case String.at(color, 5) do
+        "0" -> "R"
+        "1" -> "D"
+        "2" -> "L"
+        "3" -> "U"
+      end
+    {op, length}
   end
 end
