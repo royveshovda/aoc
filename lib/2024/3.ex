3741266a2702d43d524e807c3416637da0c9fc0e
Roy Veshovda <roy@veshovda.no>
2024-12-03 06:59:22 +0100

    D3
    Signed-off-by: Roy Veshovda <roy@veshovda.no>

--- lib/2024/3.ex
+++ lib/2024/3.ex
@@ -10,12 +10,47 @@ aoc 2024, 3 do
   """
   def p1(input) do
     input
+    |> String.split("mul(")
+    |> Enum.map(fn x -> String.split(x, ")") |> hd end)
+    |> Enum.map(fn x -> String.split(x, ",") end)
+    |> Enum.filter(fn x -> Enum.count(x) == 2 && Enum.all?(x, fn c -> is_integer_string?(c) end) end)
+    |> Enum.map(fn [x1, x2] -> String.to_integer(x1) * String.to_integer(x2) end)
+    |> Enum.sum()
+
+  end
+
+  def is_integer_string?(str) do
+    case Integer.parse(str) do
+      {_, ""} -> true
+      _ -> false
+    end
   end
 
   @doc """
       iex> p2(example_string())
   """
   def p2(input) do
-    input
+    pattern = ~r/mul\(([0-9]{1,3}),([0-9]{1,3})\)|do\(\)|don't\(\)/
+    ops =
+      Regex.scan(pattern, input)
+      |> Enum.map(fn args ->
+        case args do
+          ["do()"] -> :do
+          ["don't()"] -> :dont
+          [_, l, r] -> String.to_integer(l) * String.to_integer(r)
+        end
+      end)
+
+    ops
+    |> Enum.reduce({true, 0}, fn op, {is_on, acc} ->
+      case op do
+        :do -> {true, acc}
+        :dont -> {false, acc}
+        n when is_integer(n) and is_on -> {true, acc + n}
+        n when is_integer(n) and not(is_on) -> {false, acc}
+      end
+    end)
+    |> elem(1)
+
   end
 end
--- test/2024/3_test.exs
+++ test/2024/3_test.exs
@@ -2,18 +2,19 @@ import AOC
 
 aoc_test 2024, 3, async: true do
   test "p1e" do
-    assert Y2024.D3.p1(example_string()) == 1
+    assert Y2024.D3.p1(example_string()) == 161
   end
 
-  # test "p1i" do
-  #   assert Y2024.D3.p1(input_string()) == 2
-  # end
+  test "p1i" do
+    assert Y2024.D3.p1(input_string()) == 161085926
+  end
 
-  # test "p2e" do
-  #   assert Y2024.D3.p2(example_string()) == 3
-  # end
+  test "p2e" do
+    s2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+    assert Y2024.D3.p2(s2) == 48
+  end
 
-  # test "p2i" do
-  #   assert Y2024.D3.p2(input_string()) == 4
-  # end
+  test "p2i" do
+    assert Y2024.D3.p2(input_string()) == 82_045_421
+  end
 end
