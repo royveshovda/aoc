647967c76c9920a29dee17c10fda0adcbbb7b9b1
Roy Veshovda <roy@veshovda.no>
2024-12-05 05:07:19 +0100

    D5 prepare
    Signed-off-by: Roy Veshovda <roy@veshovda.no>

---
+++ test/2024/5_test.exs
@@ -0,0 +1,19 @@
+import AOC
+
+aoc_test 2024, 5, async: true do
+  test "p1e" do
+    assert Y2024.D5.p1(example_string()) == 0
+  end
+
+  # test "p1i" do
+  #   assert Y2024.D5.p1(input_string()) == 0
+  # end
+
+  # test "p2e" do
+  #   assert Y2024.D5.p2(example_string()) == 0
+  # end
+
+  # test "p2i" do
+  #   assert Y2024.D5.p2(input_string()) == 0
+  # end
+end
