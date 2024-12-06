6d9ee73ada4cac896fbe9e3157e69b6f69376f52
Roy Veshovda <roy@veshovda.no>
2024-12-06 06:00:21 +0100

    🚧 Prepare
    Signed-off-by: Roy Veshovda <roy@veshovda.no>

---
+++ lib/2024/6.ex
@@ -0,0 +1,21 @@
+import AOC
+
+aoc 2024, 6 do
+  @moduledoc """
+  https://adventofcode.com/2024/day/6
+  """
+
+  @doc """
+      iex> p1(example_string())
+  """
+  def p1(input) do
+    input
+  end
+
+  @doc """
+      iex> p2(example_string())
+  """
+  def p2(input) do
+    input
+  end
+end
