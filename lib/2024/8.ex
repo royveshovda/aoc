f3b91e322d0684d91f3bc164ae6345750fb0e839
Roy Veshovda <roy@veshovda.no>
2024-12-08 05:35:46 +0100

    Prepare D8
    Signed-off-by: Roy Veshovda <roy@veshovda.no>

---
+++ lib/2024/8.ex
@@ -0,0 +1,21 @@
+import AOC
+
+aoc 2024, 8 do
+  @moduledoc """
+  https://adventofcode.com/2024/day/8
+  """
+
+  @doc """
+      iex> p1(example_string())
+  """
+  def p1(input) do
+    input
+  end
+
+  @doc """
+      iex> p2(example_string())
+  """
+  def p2(input) do
+    input
+  end
+end
