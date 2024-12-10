1bc0406280b1c3175d0c03eb963bf0ca65dde77e
Roy Veshovda <roy@veshovda.no>
2024-12-10 06:54:10 +0100

    ✨ D10
    Signed-off-by: Roy Veshovda <roy@veshovda.no>

--- lib/2024/10.ex
+++ lib/2024/10.ex
@@ -6,10 +6,65 @@ aoc 2024, 10 do
   """
 
   def p1(input) do
-    input
+    grid = Utils.Grid.input_to_map(input, &String.to_integer/1)
+    starts = grid |> Enum.filter(fn {_pos, v} -> v == 0 end)
+
+    # {start_0_pos, start_0_value} = Enum.at(starts, 0)
+    # {start_1_pos, start_1_value} = Enum.at(starts, 1)
+
+    # next_step(grid, start_0_pos, start_0_value)
+    # next_step(grid, start_1_pos, start_1_value)
+
+    starts
+    |> Enum.map(fn {pos, value} -> next_step_p1(grid, pos, value) end)
+    |> Enum.map(fn x -> Enum.count(x) end)
+    |> Enum.sum()
+
+
+
+  end
+
+  def next_step_p1(_grid, pos, 9), do: [pos]
+
+  def next_step_p1(grid, pos, value) do
+    #IO.inspect("Step at #{inspect(pos)} with value #{value}")
+    {x, y} = pos
+    p1 = {x + 1, y}
+    p2 = {x - 1, y}
+    p3 = {x, y + 1}
+    p4 = {x, y - 1}
+
+    [p1, p2, p3, p4]
+    |> Enum.map(fn p -> {p, grid[p]} end)
+    |> Enum.filter(fn {_p, v} -> v == value + 1 end)
+    |> Enum.flat_map(fn {p, v} -> next_step_p1(grid, p, v) end)
+    |> Enum.uniq()
+  end
+
+  def next_step_p2(_grid, _pos, 9), do: 1
+
+  def next_step_p2(grid, pos, value) do
+    #IO.inspect("Step at #{inspect(pos)} with value #{value}")
+    {x, y} = pos
+    p1 = {x + 1, y}
+    p2 = {x - 1, y}
+    p3 = {x, y + 1}
+    p4 = {x, y - 1}
+
+    [p1, p2, p3, p4]
+    |> Enum.map(fn p -> {p, grid[p]} end)
+    #|> IO.inspect()
+    |> Enum.filter(fn {_p, v} -> v == value + 1 end)
+    |> Enum.map(fn {p, v} -> next_step_p2(grid, p, v) end)
+    |> Enum.sum()
   end
 
   def p2(input) do
-    input
+    grid = Utils.Grid.input_to_map(input, &String.to_integer/1)
+    starts = grid |> Enum.filter(fn {_pos, v} -> v == 0 end)
+
+    starts
+    |> Enum.map(fn {pos, value} -> next_step_p2(grid, pos, value) end)
+    |> Enum.sum()
   end
 end
--- test/2024/10_test.exs
+++ test/2024/10_test.exs
@@ -1,19 +1,58 @@
 import AOC
 
 aoc_test 2024, 10, async: true do
-  # test "p1e" do
-  #   assert Y2024.D10.p1(example_string()) == 1
-  # end
 
-  # test "p1i" do
-  #   assert Y2024.D10.p1(input_string()) == 1
-  # end
+  def get_example_input_1() do
+    """
+    4320234
+    5441987
+    9992999
+    6543456
+    7911117
+    8111118
+    9111119
+    """
+  end
 
-  # test "p2e" do
-  #   assert Y2024.D10.p2(example_string()) == 1
-  # end
+  def get_example_input_2() do
+    """
+    1190229
+    3331398
+    4442447
+    6543456
+    7651987
+    8761111
+    9871111
+    """
+  end
 
-  # test "p2i" do
-  #   assert Y2024.D10.p2(input_string()) == 1
-  # end
+  def get_example_input_3() do
+    """
+    89010123
+    78121874
+    87430965
+    96549874
+    45678903
+    32019012
+    01329801
+    10456732
+    """
+  end
+  test "p1e" do
+    assert Y2024.D10.p1(get_example_input_1()) == 2
+    assert Y2024.D10.p1(get_example_input_2()) == 4
+    assert Y2024.D10.p1(get_example_input_3()) == 36
+  end
+
+  test "p1i" do
+    assert Y2024.D10.p1(input_string()) == 744
+  end
+
+  test "p2e" do
+    assert Y2024.D10.p2(get_example_input_3()) == 81
+  end
+
+  test "p2i" do
+    assert Y2024.D10.p2(input_string()) == 1651
+  end
 end
