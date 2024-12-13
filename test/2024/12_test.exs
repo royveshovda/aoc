8df75e6ad2da9fe969f2ff125e31775ba7261869
Roy Veshovda <roy@veshovda.no>
2024-12-13 03:53:38 +0100

    D12
    Signed-off-by: Roy Veshovda <roy@veshovda.no>

--- test/2024/12_test.exs
+++ test/2024/12_test.exs
@@ -1,19 +1,33 @@
 import AOC
 
 aoc_test 2024, 12, async: true do
-  # test "p1e" do
-  #   assert Y2024.D12.p1(example_string()) == 1
-  # end
+  def get_example_input() do
+    """
+    RRRRIICCFF
+    RRRRIICCCF
+    VVRRRCCFFF
+    VVRCCCJFFF
+    VVVVCJJCFE
+    VVIVCCJJEE
+    VVIIICJJEE
+    MIIIIIJJEE
+    MIIISIJEEE
+    MMMISSJEEE
+    """
+  end
+  test "p1e" do
+    assert Y2024.D12.p1(get_example_input()) == 1930
+  end
 
-  # test "p1i" do
-  #   assert Y2024.D12.p1(input_string()) == 1
-  # end
+  test "p1i" do
+    assert Y2024.D12.p1(input_string()) == 1437300
+  end
 
-  # test "p2e" do
-  #   assert Y2024.D12.p2(example_string()) == 1
-  # end
+  test "p2e" do
+    assert Y2024.D12.p2(get_example_input()) == 1206
+  end
 
-  # test "p2i" do
-  #   assert Y2024.D12.p2(input_string()) == 1
-  # end
+  test "p2i" do
+    assert Y2024.D12.p2(input_string()) == 849332
+  end
 end
