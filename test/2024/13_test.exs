301c18dd9af1fad9a3dd817afb040358106e61a7
Roy Veshovda <roy@veshovda.no>
2024-12-13 06:52:37 +0100

    ✨ D13
    Signed-off-by: Roy Veshovda <roy@veshovda.no>

--- test/2024/13_test.exs
+++ test/2024/13_test.exs
@@ -1,19 +1,19 @@
 import AOC
 
 aoc_test 2024, 13, async: true do
-  # test "p1e" do
-  #   assert Y2024.D13.p1(example_string()) == 1
-  # end
+  test "p1e" do
+    assert Y2024.D13.p1(example_string()) == 480
+  end
 
-  # test "p1i" do
-  #   assert Y2024.D13.p1(input_string()) == 1
-  # end
+  test "p1i" do
+    assert Y2024.D13.p1(input_string()) == 37_686
+  end
 
-  # test "p2e" do
-  #   assert Y2024.D13.p2(example_string()) == 1
-  # end
+  test "p2e" do
+    assert Y2024.D13.p2(example_string()) == 875_318_608_908
+  end
 
-  # test "p2i" do
-  #   assert Y2024.D13.p2(input_string()) == 1
-  # end
+  test "p2i" do
+    assert Y2024.D13.p2(input_string()) == 77_204_516_023_437
+  end
 end
