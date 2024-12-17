40c2e50e6bc5badbad86dc73a56ea4d8aabf282b
Roy Veshovda <roy@veshovda.no>
2024-12-17 06:00:34 +0100

    D16
    Signed-off-by: Roy Veshovda <roy@veshovda.no>

--- test/2024/16_test.exs
+++ test/2024/16_test.exs
@@ -1,19 +1,19 @@
 import AOC
 
 aoc_test 2024, 16, async: true do
-  # test "p1e" do
-  #   assert Y2024.D16.p1(example_string()) == 1
-  # end
+  test "p1e" do
+    assert Y2024.D16.p1(example_string()) == 7036
+  end
 
-  # test "p1i" do
-  #   assert Y2024.D16.p1(input_string()) == 1
-  # end
+  test "p1i" do
+    assert Y2024.D16.p1(input_string()) == 94436
+  end
 
-  # test "p2e" do
-  #   assert Y2024.D16.p2(example_string()) == 1
-  # end
+  test "p2e" do
+    assert Y2024.D16.p2(example_string()) == 45
+  end
 
-  # test "p2i" do
-  #   assert Y2024.D16.p2(input_string()) == 1
-  # end
+  test "p2i" do
+    assert Y2024.D16.p2(input_string()) == 481
+  end
 end
