6493c5760bfc39f5419bffe0dc798d2ff86b8c58
Roy Veshovda <roy@veshovda.no>
2024-12-18 06:00:15 +0100

    D17
    Signed-off-by: Roy Veshovda <roy@veshovda.no>

--- test/2024/17_test.exs
+++ test/2024/17_test.exs
@@ -1,19 +1,63 @@
 import AOC
 
 aoc_test 2024, 17, async: true do
-  # test "p1e" do
-  #   assert Y2024.D17.p1(example_string()) == 1
+  def get_example_string_1() do
+    """
+    Register A: 0
+    Register B: 0
+    Register C: 9
+
+    Program: 2,6
+    """
+  end
+
+  def get_example_string_2() do
+    # If register A contains 10, the program 5,0,5,1,5,4 would output 0,1,2.
+    """
+    Register A: 10
+    Register B: 0
+    Register C: 9
+
+    Program: 0,1,5,4,3,0
+    """
+  end
+
+  def get_example_string_3() do
+    # If register A contains 2024, the program 0,1,5,4,3,0 would output 4,2,5,6,7,7,7,7,3,1,0 and leave 0 in register A.
+    """
+    Register A: 2024
+    Register B: 0
+    Register C: 9
+
+    Program: 0,1,5,4,3,0
+    """
+  end
+
+  test "p1e_1" do
+    assert Y2024.D17.p1(get_example_string_1()) == ""
+  end
+
+  # test "p1e_2" do
+  #   assert Y2024.D17.p1(get_example_string_2()) == "0,1,2"
   # end
 
-  # test "p1i" do
-  #   assert Y2024.D17.p1(input_string()) == 1
-  # end
+  test "p1e_3" do
+    assert Y2024.D17.p1(get_example_string_3()) == "4,2,5,6,7,7,7,7,3,1,0"
+  end
 
-  # test "p2e" do
-  #   assert Y2024.D17.p2(example_string()) == 1
-  # end
+  test "p1e" do
+    assert Y2024.D17.p1(example_string()) == "4,6,3,5,6,3,5,2,1,0"
+  end
 
-  # test "p2i" do
-  #   assert Y2024.D17.p2(input_string()) == 1
-  # end
+  test "p1i" do
+    assert Y2024.D17.p1(input_string()) == "3,4,3,1,7,6,5,6,0"
+  end
+
+  test "p2e" do
+    assert Y2024.D17.p2(example_string()) == 117440
+  end
+
+  test "p2i" do
+    assert Y2024.D17.p2(input_string()) == 1
+  end
 end
