45a5f6718cf869ab7898d873dba1385286a26162
Roy Veshovda <roy@veshovda.no>
2024-12-19 06:26:46 +0100

    D18
    Signed-off-by: Roy Veshovda <roy@veshovda.no>

--- lib/2024/18.ex
+++ lib/2024/18.ex
@@ -5,11 +5,97 @@ aoc 2024, 18 do
   https://adventofcode.com/2024/day/18
   """
 
-  def p1(input) do
-    input
+  def p1({input, read_length, size}) do
+    # def part1(input, max \\ 70, n \\ 1024) do
+    grid = parse(input)
+    |> Enum.take(read_length)
+    |> MapSet.new
+    grid = {grid, 0..size}
+    start = {0, 0}
+    goal = {size, size}
+    q = q_new({0, start})
+    seen = MapSet.new([start])
+    walk(q, goal, grid, seen)
   end
 
-  def p2(input) do
+
+  def p2({input, read_length, size}) do
+    {first, rest} = Enum.split(parse(input), read_length)
+    grid = {MapSet.new(first), 0..size}
+    start = {0, 0}
+    goal = {size, size}
+    q = q_new({0, start})
+    seen = MapSet.new([start])
+
+    pos =
+      Enum.reduce_while(rest, grid, fn next, {grid, r} ->
+        grid = {MapSet.put(grid, next), r}
+        case walk(q, goal, grid, seen) do
+          nil ->
+            {:halt, next}
+          _steps ->
+            {:cont, grid}
+        end
+      end)
+
+    {x, y} = pos
+    "#{x},#{y}"
+  end
+
+  defp walk(q, goal, grid, seen) do
+    case q_take_smallest(q) do
+      nil ->
+        nil
+      {{steps, current}, q} ->
+        new = adjacent_squares(current)
+        |> Enum.reject(fn next ->
+          blocked?(next, grid) or MapSet.member?(seen, next)
+        end)
+        if Enum.member?(new, goal) do
+          steps + 1
+        else
+          seen = MapSet.union(seen, MapSet.new(new))
+          q = Enum.reduce(new, q, fn next, q ->
+            q_put(q, {steps + 1, next})
+          end)
+          walk(q, goal, grid, seen)
+        end
+    end
+  end
+
+  defp q_new(e) do
+    :queue.from_list([e])
+  end
+
+  defp q_put(q, e) do
+    :queue.in(e, q)
+  end
+
+  defp q_take_smallest(q) do
+    case :queue.is_empty(q) do
+      true ->
+        nil
+      false ->
+        {:queue.get(q), :queue.drop(q)}
+    end
+  end
+
+  defp blocked?({row, col}, {grid, r}) do
+    (row not in r) or (col not in r) or
+    MapSet.member?(grid, {row, col})
+  end
+
+  defp adjacent_squares({row, col}) do
+    [{row - 1, col}, {row, col - 1}, {row, col + 1}, {row + 1, col}]
+  end
+
+  defp parse(input) do
     input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      String.split(line, ",")
+      |> Enum.map(&String.to_integer/1)
+      |> List.to_tuple
+    end)
   end
 end
--- test/2024/18_test.exs
+++ test/2024/18_test.exs
@@ -1,19 +1,19 @@
 import AOC
 
 aoc_test 2024, 18, async: true do
-  # test "p1e" do
-  #   assert Y2024.D18.p1(example_string()) == 1
-  # end
+  test "p1e" do
+    assert Y2024.D18.p1({example_string(), 12, 6}) == 22
+  end
 
-  # test "p1i" do
-  #   assert Y2024.D18.p1(input_string()) == 1
-  # end
+  test "p1i" do
+    assert Y2024.D18.p1({input_string(), 1024, 70}) == 250
+  end
 
-  # test "p2e" do
-  #   assert Y2024.D18.p2(example_string()) == 1
-  # end
+  test "p2e" do
+    assert Y2024.D18.p2({example_string(), 12, 6}) == "6,1"
+  end
 
-  # test "p2i" do
-  #   assert Y2024.D18.p2(input_string()) == 1
-  # end
+  test "p2i" do
+    assert Y2024.D18.p2({input_string(), 1024, 70}) == "56,8"
+  end
 end
