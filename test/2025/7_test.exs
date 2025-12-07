dc322764235aa7d58b2e5dde7ad3574b3cfa1e81
Roy Veshovda <roy@veshovda.no>
2025-12-07 06:13:12 +0100

    fix test for day 7

--- test/2025/7_test.exs
+++ test/2025/7_test.exs
@@ -1,11 +1,14 @@
-import AOC
+defmodule Y2025.D7Test do
+  use ExUnit.Case, async: true
+  import AOC
 
-aoc_test 2025, 7, async: true do
-  test "part 1 with input" do
-    assert Y2025.D7.p1(input_string()) == 1579
-  end
+  aoc_test 2025, 7, async: true do
+    test "part 1 with input" do
+      assert Y2025.D7.p1(input_string()) == 1579
+    end
 
-  test "part 2 with input" do
-    assert Y2025.D7.p2(input_string()) == 13_418_215_871_354
+    test "part 2 with input" do
+      assert Y2025.D7.p2(input_string()) == 13_418_215_871_354
+    end
   end
 end
